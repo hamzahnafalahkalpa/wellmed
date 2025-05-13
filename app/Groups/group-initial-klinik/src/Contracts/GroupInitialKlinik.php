afd68ef35ff2da7a26c94deb158d58912e040443
Hamzah <hamzahnafalah@gmail.com>
2025-05-13 08:53:30 +0700

    modules

--- app/Groups/group-initial-klinik/src/Contracts/GroupInitialKlinik.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Klinik\GroupInitialKlinik\Contracts;
-
-use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
-
-interface GroupInitialKlinik extends DataManagement{
-
-}
--- app/Projects/klinik/src/Models/MicroTenant/Tenant.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Projects\Klinik\Models\MicroTenant;
-
-use Hanafalah\MicroTenant\Models\Tenant\Tenant as TenantTenant;
-
-class Tenant extends TenantTenant{
-    protected $db_name = 'central';
-}
