9684d1d38bcc6764537e49cfc3e7a18db8e227d3
Hamzah <hamzahnafalah@gmail.com>
2025-08-25 14:14:26 +0700

    update docker file

--- docker/development/entrypoint.sh
+++ docker/development/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-USER_ID=${UID:-1000}
-GROUP_ID=${GID:-1000}
+# USER_ID=${UID:-1000}
+# GROUP_ID=${GID:-1000}
 
 # echo "Fixing file permissions..."
 # chown -R ${USER_ID}:${GROUP_ID} /app/storage /app/bootstrap/cache /app/public/frankenphp-worker.php || echo "Some files could not be changed"
