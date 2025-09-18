4b1734dd23f1374750311a4579cda93d3ea7362f
Hamzah <hamzahnafalah@gmail.com>
2025-09-18 22:18:04 +0700

    Pemisahan composer untuk lite, plus dan hq

---
+++ docker/development/entrypoint.lite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "==> Entrypoint Lite running..."
+
+# masuk ke root project
+cd /app/projects/wellmed-lite
+
+# composer install untuk root project
+echo "==> Running composer install in /app..."
+composer install
+cd /app
+
+# kalau ada migrasi, cache, dll bisa ditambah
+# php artisan migrate --force
+# php artisan config:cache
+# php artisan route:cache
+
+# terakhir jalankan php-fpm
+exec "$@"
--- docker/development/entrypoint.sh
+++ docker/development/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 set -e
 
-# USER_ID=${UID:-1000}
-# GROUP_ID=${GID:-1000}
-
-# echo "Fixing file permissions..."
-# chown -R ${USER_ID}:${GROUP_ID} /app/storage /app/bootstrap/cache /app/public/frankenphp-worker.php || echo "Some files could not be changed"
-
-# echo "Clearing caches..."
-# php artisan optimize:clear
-
 exec "$@"
