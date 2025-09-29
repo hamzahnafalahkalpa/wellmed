f4428afc36df0027fa1d9f6a9ae4f05ed4115fe7
hamzah <hamzahnafalah@gmail.com>
2025-09-29 15:09:38 +0700

    update file upload and hq

---
+++ app/Http/Controllers/API/ApiAccess/HqApiAccessController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API\ApiAccess;
+
+use App\Http\Requests\API\ApiAccess\{
+    StoreRequest, DeleteRequest
+};
+use App\Http\Resources\API\ApiAccess\HqGenerateTokenResponse;
+use Hanafalah\ApiHelper\Facades\ApiAccess;
+use Hanafalah\MicroTenant\Facades\MicroTenant;
+use Illuminate\Support\Facades\Auth;
+
+class ApiAccessController extends EnvironmentController{
+    
+    public function store(StoreRequest $request){
+        $token = $this->generateToken();
+        if (isset($token) && request()->headers->has('AppCode')) {
+            ApiAccess::init($token)->accessOnLogin(function ($api_access) {
+                MicroTenant::onLogin($api_access);
+            });
+        }
+        $user        = $this->getUser();
+        $user->token = $token;
+        return (new HqGenerateTokenResponse($user))->resolve();
+    }
+
+    public function destroy(DeleteRequest $request){
+        Auth::logout();
+        return true;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,30 +6,34 @@ use Illuminate\Support\Facades\Route;
 use Hanafalah\LaravelSupport\Facades\LaravelSupport;
 
 
-// $hq_domains = [
-//     'localhost:8007'
-// ];
+$hq_domains = [
+    'localhost:8007'
+];
 
-// foreach ($hq_domains as $hq_domain) {
-//     Route::domain($hq_domain)->group(function () {
-//         ApiAccess::secure(function(){
-//             Route::apiResource('token',HqApiAccessController::class)
-//                 ->only('store','destroy')
-//                 ->parameters(['token' => 'uuid']);
-//         }); 
-//     });
-// }
+foreach ($hq_domains as $hq_domain) {
+    Route::domain($hq_domain)->group(function () {
+        ApiAccess::secure(function(){
+            Route::apiResource('token',HqApiAccessController::class)
+                ->only('store','destroy')
+                ->parameters(['token' => 'uuid']);
+        }); 
+    });
+}
 
 $wellmed_lite_domains = [
     'localhost:8005',
-    'wellmed-'
+    env('WELLMED_CORE_DEV_URL','wellmed-core.kalpahealth.com')
 ];
 
-ApiAccess::secure(function(){
-    Route::apiResource('token',ApiAccessController::class)
-        ->only('store','destroy')
-        ->parameters(['token' => 'uuid']);
-}); 
+foreach ($wellmed_lite_domains as $wellmed_lite_domain) {
+    Route::domain($hq_domain)->group(function () {
+        ApiAccess::secure(function(){
+            Route::apiResource('token',ApiAccessController::class)
+                ->only('store','destroy')
+                ->parameters(['token' => 'uuid']);
+        }); 
+    });
+}
 
 Route::group([
     'as' => 'api.'
