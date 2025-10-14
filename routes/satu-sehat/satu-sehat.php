6ce6f3e7b217b6cfdbcbeb70344ed1a763a5e618
hamzah <hamzahnafalah@gmail.com>
2025-10-14 15:20:20 +0700

    satu sehat param

--- routes/satu-sehat/satu-sehat.php
+++ routes/satu-sehat/satu-sehat.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Projects\WellmedLite\Controllers\API\SatuSehat\{
+    Autolist\AutolistController,
     AuthController
 };
 
 Route::apiResource('/token',AuthController::class)->only('store');
+Route::apiResource('/autolist/{morph}/{type}',AutolistController::class)->only('index');
