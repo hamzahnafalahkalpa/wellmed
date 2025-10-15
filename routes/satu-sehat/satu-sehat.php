26e7e11905e758b73e1e2464dfc50b3997224764
hamzah <hamzahnafalah@gmail.com>
2025-10-15 16:38:50 +0700

    update visit registration

--- routes/satu-sehat/satu-sehat.php
+++ routes/satu-sehat/satu-sehat.php
@@ -5,6 +5,8 @@ use Projects\WellmedLite\Controllers\API\SatuSehat\{
     Autolist\AutolistController,
     AuthController
 };
+use Projects\WellmedLite\Controllers\API\SatuSehat\Patient\PatientController;
 
 Route::apiResource('/token',AuthController::class)->only('store');
+Route::apiResource('/patient',PatientController::class)->only('store');
 Route::apiResource('/autolist/{morph}/{type}',AutolistController::class)->only('index');
