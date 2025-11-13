2289bceb2b0a6a4809255415c62179728ec6ffbb
Hamzah <hamzah@kalpahealth.com>
2025-11-13 11:43:47 +0700

    updat emodules

--- routes/api-lite.php
+++ routes/api-lite.php
@@ -1,18 +1,9 @@
 <?php
 
 use App\Http\Controllers\API\ApiAccess\ApiAccessController;
-use App\Http\Controllers\API\ApiAccess\HqApiAccessController;
 use Hanafalah\ApiHelper\Facades\ApiAccess;
 use Illuminate\Support\Facades\Route;
 use Hanafalah\LaravelSupport\Facades\LaravelSupport;
-use Projects\WellmedLite\Controllers\API\SatuSehat\{
-    Autolist\AutolistController,
-    AuthController
-};
-use Projects\WellmedLite\Controllers\API\SatuSehat\{
-    Organization\OrganizationController,
-    Patient\PatientController
-};
 
 ApiAccess::secure(function(){
     Route::apiResource('token',ApiAccessController::class)
