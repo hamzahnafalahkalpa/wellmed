e4646ef794f5358cad699df644c0a702004ab353
Hamzah <hamzah@kalpahealth.com>
2025-11-18 13:59:50 +0700

    update practitioner

--- docker/development/entrypoint.backbone.sh
+++ docker/development/entrypoint.backbone.sh
@@ -17,26 +17,26 @@ echo "APP_ENV = ${APP_ENV:-development}"
 # -------------------------
 # Opcache setup (CLI untuk Octane)
 # -------------------------
-PHP_OPCACHE_CONF=/usr/local/etc/php/conf.d/00-opcache.ini
+# PHP_OPCACHE_CONF=/usr/local/etc/php/conf.d/00-opcache.ini
 
-if [ ! -f "$PHP_OPCACHE_CONF" ]; then
-  echo "==> Creating Opcache CLI configuration..."
-  cat <<EOL > $PHP_OPCACHE_CONF
-opcache.enable=1
-opcache.enable_cli=1
-opcache.memory_consumption=256
-opcache.interned_strings_buffer=16
-opcache.max_accelerated_files=10000
-opcache.validate_timestamps=0
-opcache.revalidate_freq=0
-EOL
-fi
+# if [ ! -f "$PHP_OPCACHE_CONF" ]; then
+#   echo "==> Creating Opcache CLI configuration..."
+#   cat <<EOL > $PHP_OPCACHE_CONF
+# opcache.enable=1
+# opcache.enable_cli=1
+# opcache.memory_consumption=256
+# opcache.interned_strings_buffer=16
+# opcache.max_accelerated_files=10000
+# opcache.validate_timestamps=0
+# opcache.revalidate_freq=0
+# EOL
+# fi
 
-# -------------------------
-# Upload size config
-# -------------------------
-echo "upload_max_filesize=10M" > /usr/local/etc/php/conf.d/99-upload.ini
-echo "post_max_size=10M" >> /usr/local/etc/php/conf.d/99-upload.ini
+# # -------------------------
+# # Upload size config
+# # -------------------------
+# echo "upload_max_filesize=10M" > /usr/local/etc/php/conf.d/99-upload.ini
+# echo "post_max_size=10M" >> /usr/local/etc/php/conf.d/99-upload.ini
 
 # -------------------------
 # ENV-based config with fallback
