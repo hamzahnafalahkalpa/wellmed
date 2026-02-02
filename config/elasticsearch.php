5d7ec56f423395b10bc850f39375d28cf4f867b9
Hamzah <hamzah@kalpahealth.com>
2026-02-02 12:36:37 +0700

    -m

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -12,8 +12,7 @@ return [
     | will fall back to standard database queries.
     |
     */
-    // 'enabled' => env('ELASTICSEARCH_ENABLED', true),
-    'enabled' => false,
+    'enabled' => env('ELASTICSEARCH_ENABLED', true),
 
     'job_class' => ElasticJob::class,
 
